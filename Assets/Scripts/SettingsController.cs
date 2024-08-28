aa74f18c6e6f2e64c9329c735a295f9ea8f24f5a
jidaprano <joseph.daprano@cadtalk.com>
2024-08-28 15:59:49 -0400

    start modular settings system

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -1,38 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    public AudioSource audioSource;
-    public TMPro.TMP_Dropdown resolutionDropdown;
-    public Slider volumeSlider;
-    public Toggle fullscreenToggle;
-    public Button doneButton;
+    public SettingsObject[] settingsObjects;
     float currVolume;
     Resolution[] resolutions;
+    public GameObject settingsTitle;
 
     // Start is called before the first frame update
     void Start()
     {
-        resolutionDropdown.ClearOptions();
-        List<string> resolutionOptions = new List<string>();
-        resolutions = Screen.resolutions;
-        int currentResolutionIndex = 0;
-        for(int i = 0; i < resolutions.Length; i++)
+        for(int i = 0; i < settingsObjects.Length; i++)
         {
-            string resolutionOption = resolutions[i].width + " x " + resolutions[i].height;
-            resolutionOptions.Add(resolutionOption);
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
+            settingsObjects[i].CreateUIElement(settingsTitle, i);
         }
-        resolutionDropdown.AddOptions(resolutionOptions);
-        resolutionDropdown.RefreshShownValue();
-        LoadSettings(currentResolutionIndex);
+        //resolutionDropdown.ClearOptions();
+        //List<string> resolutionOptions = new List<string>();
+        //resolutions = Screen.resolutions;
+        //int currentResolutionIndex = 0;
+        //for(int i = 0; i < resolutions.Length; i++)
+        //{
+        //    string resolutionOption = resolutions[i].width + " x " + resolutions[i].height;
+        //    resolutionOptions.Add(resolutionOption);
+        //    if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+        //    {
+        //        currentResolutionIndex = i;
+        //    }
+        //}
+        //resolutionDropdown.AddOptions(resolutionOptions);
+        //resolutionDropdown.RefreshShownValue();
+        //LoadSettings(currentResolutionIndex);
     }
     public void SetFullscreen(bool isFullscreen)
     {
@@ -41,7 +44,7 @@ public class SettingsController : MonoBehaviour
 
     public void SetVolume(float vol)
     {
-        audioSource.volume = vol;
+        //audioSource.volume = vol;
         currVolume = vol;
     }
 
@@ -53,21 +56,94 @@ public class SettingsController : MonoBehaviour
 
     public void SaveSettings()
     {
-        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        //PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
         PlayerPrefs.SetFloat("VolumePreference", currVolume);
     }
 
     public void LoadSettings(int currentResolutionIndex)
     {
-        if (PlayerPrefs.HasKey("ResolutionPreference"))
+        //if (PlayerPrefs.HasKey("ResolutionPreference"))
+        //{
+        //    resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        //}
+        //else
+        //{
+        //    resolutionDropdown.value = currentResolutionIndex;
+        //}
+
+        //volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+    }
+
+    [System.Serializable]
+    public class SettingsObject
+    {
+        public enum UIElementType { Dropdown, Slider, Toggle };
+
+        [System.Serializable]
+        public struct UIElementConfig
         {
-            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
-        }
-        else
-        {
-            resolutionDropdown.value = currentResolutionIndex;
+            public UIElementType elementType;
+            public string label;
+
+            public Action<TMPro.TMP_Dropdown> dropdownSetup;
+            public Action<Slider> sliderSetup;
+            public Action<Toggle> toggleSetup;
         }
 
-        volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        public UIElementConfig config;
+
+        public TMPro.TMP_Dropdown dropdownPrefab;
+        public Slider sliderPrefab;
+        public Toggle togglePrefab;
+        public Button buttonPrefab;
+
+        public void CreateUIElement(GameObject parent, int listIndex)
+        {
+            GameObject uiElement = null;
+
+            switch (config.elementType)
+            {
+                case UIElementType.Dropdown:
+                    uiElement = Instantiate(dropdownPrefab.gameObject, parent.gameObject.transform);
+                    SetupDropdown(uiElement.GetComponent<TMPro.TMP_Dropdown>());
+                    break;
+                case UIElementType.Slider:
+                    uiElement = Instantiate(sliderPrefab.gameObject, parent.gameObject.transform);
+                    SetupSlider(uiElement.GetComponent<Slider>());
+                    break;
+                case UIElementType.Toggle:
+                    uiElement = Instantiate(togglePrefab.gameObject, parent.gameObject.transform);
+                    SetupToggle(uiElement.GetComponent<Toggle>());
+                    break;
+            }
+
+            if(uiElement != null)
+            {
+                RectTransform newRectTransform = uiElement.GetComponent<RectTransform>();
+                Vector2 newPosition = uiElement.GetComponent<RectTransform>().anchoredPosition;
+                newPosition.y -= (50 * listIndex);
+                newRectTransform.anchoredPosition = newPosition;
+            }
+        }
+
+        private void SetupDropdown(TMPro.TMP_Dropdown dropdown)
+        {
+            dropdown.options.Clear();
+            config.dropdownSetup?.Invoke(dropdown);
+        }
+
+        private void SetupSlider(Slider slider)
+        {
+            slider.minValue = 0;
+            slider.maxValue = 100;
+            slider.value = 50;
+            config.sliderSetup?.Invoke(slider);
+        }
+
+        private void SetupToggle(Toggle toggle)
+        {
+            toggle.isOn = false;
+            config.toggleSetup?.Invoke(toggle);
+        }
     }
 }
