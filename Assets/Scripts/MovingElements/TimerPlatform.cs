9edd9f9c68a3ba92f73e1632096323d965937fa4
Cadence Hagenauer <123522477+cadenceblorbo@users.noreply.github.com>
2024-08-29 20:56:03 -0400

    im up trying my angles

--- Assets/Scripts/MovingElements/TimerPlatform.cs
+++ Assets/Scripts/MovingElements/TimerPlatform.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Utility;
 using MIDI2EventSystem;
 using System;
+using System.Drawing;
 
 public class TimerPlatform : MonoBehaviour
 {
@@ -11,13 +12,13 @@ public class TimerPlatform : MonoBehaviour
     public Material OnMaterial;
 
     [SerializeField]
-    public float TriangleRadius;
+    public float TriangleCalculationRadius;
 
     [SerializeField]
-    public float TriangleCenterOffset;
+    public float TrianglePushout;
 
     [SerializeField]
-    public Vector3 TriangleOffset;
+    public Vector3 TriangleCenterOffset;
 
     [SerializeField]
     [Range(1, 179)]
@@ -32,66 +33,80 @@ public class TimerPlatform : MonoBehaviour
     [SerializeField]
     internal NoteDebounceTriplet[] Debounces;
 
-    private Mesh[] triangles;
+    private Mesh[][] triangleRings;
     private RenderParams onParams;
+    private int activeRing = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         onParams = new RenderParams(OnMaterial);
-        triangles = GenerateTriangleRing(5);
+        triangleRings = new Mesh[Debounces.Length][];
+        for(int i = 0; i<triangleRings.Length; i++)
+        {
+            triangleRings[i] = GenerateTriangleRing(Debounces[i].numTicks);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < triangles.Length; i++)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Graphics.RenderMesh(onParams, triangles[i], 0, transform.localToWorldMatrix);
+            activeRing = (activeRing + 1) % triangleRings.Length;
+        }
+        
+        for (int i = 0; i < triangleRings[activeRing].Length; i++)
+        {
+            Graphics.RenderMesh(onParams, triangleRings[activeRing][i], 0, transform.localToWorldMatrix);
         }
     }
 
     public Mesh[] GenerateTriangleRing(int numTriangles) {
         Mesh[] result = new Mesh[numTriangles];
         float radAngle = 2 * Mathf.PI / numTriangles;
+        float maxRadAngle = MaxInternalTriangleAngle * Mathf.Deg2Rad;
+        float angleForTris = Mathf.Min(radAngle, maxRadAngle);
 
         for (int i = 0; i<numTriangles; i++)
         {
             Vector3[] points = new Vector3[3];
 
             //generate triangle points
-            points[0] = Vector3.zero + TriangleOffset;
+            points[0] = Vector3.zero + TriangleCenterOffset;
 
-            float halfFarSideLen = Mathf.Sin(radAngle / 2) * TriangleRadius;
-            float height = Mathf.Cos(radAngle / 2) * TriangleRadius;
+            //
+
+            float halfFarSideLen = Mathf.Sin(angleForTris / 2) * TriangleCalculationRadius;
+            float height = Mathf.Cos(angleForTris / 2) * TriangleCalculationRadius;
 
             points[1] = points[0] + new Vector3(-halfFarSideLen, 0, height);
             points[2] = points[0] + new Vector3(halfFarSideLen, 0, height);
 
             //rotate triangle points
-            
-            for(int j = 0; j< 3; j++)
+            for (int j = 0; j< 3; j++)
             {
                 float rotAngle = radAngle * i;
                 float oldX = points[j].x;
                 points[j].x = points[j].x * Mathf.Cos(rotAngle) - points[j].z * Mathf.Sin(rotAngle);
                 points[j].z = oldX * Mathf.Sin(rotAngle) + points[j].z * Mathf.Cos(rotAngle);
             }
+
             
-            
-            foreach (Vector3 point in points)
+            //pushout triangle points
+            Vector3 unitPush = (points[1] + points[2]).normalized;
+            for (int j = 0; j < 3; j++)
             {
-                Debug.Log(point);
+                points[j] += unitPush * TrianglePushout;
             }
+            
 
             //make triangle mesh
             Mesh newMesh = TriangleMaker.MakeTriangle(points);
             result[i] = newMesh;
         }
 
-        
         return result;
-
     }
 
 
