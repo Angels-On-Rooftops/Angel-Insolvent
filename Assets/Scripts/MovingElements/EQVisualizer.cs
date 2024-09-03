b5bb054e05f70ad96ee40d533d6ded2d7d6df958
Cadence Hagenauer <123522477+cadenceblorbo@users.noreply.github.com>
2024-09-03 17:09:40 -0400

    eq columns made

--- Assets/Scripts/MovingElements/EQVisualizer.cs
+++ Assets/Scripts/MovingElements/EQVisualizer.cs
@@ -9,10 +9,19 @@ public class EQVisualizer : MonoBehaviour
     AudioSource Audio;
 
     [SerializeField]
-    int Bins;
+    GameObject EQBlock;
 
     [SerializeField]
-    int BinHeight;
+    GameObject ScaleRef;
+
+    [SerializeField]
+    int NumBins;
+
+    [SerializeField]
+    float BinMin = 0;
+
+    [SerializeField]
+    float BinMax = 1;
 
     [SerializeField]
     [Range(6, 13)]
@@ -22,17 +31,43 @@ public class EQVisualizer : MonoBehaviour
     FFTWindow WindowType = FFTWindow.Rectangular;
 
     float[] Spectrum;
+    float[] Bins;
+    GameObject[] BinObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         Spectrum = new float[(int)Mathf.Pow(2, SampleArrayPower)];
+        Bins = new float[NumBins];
+        BinObjects = new GameObject[NumBins];
+        MakeBins();
     }
 
     // Update is called once per frame
     void Update()
     {
         Audio.GetSpectrumData(Spectrum, 0, WindowType);
-        Debug.Log(string.Join(",", Spectrum));
+        //Debug.Log(string.Join(",", Spectrum));
+    }
+
+    void MakeBins()
+    {
+        float EQWidth = ScaleRef.transform.localScale.x;
+        Vector3 instanceScale = new Vector3(EQWidth / NumBins, 1, ScaleRef.transform.position.z);
+        for (int i = 0; i<Bins.Length; i++)
+        {
+            
+
+            //create block instance
+            GameObject currInstance = Instantiate(EQBlock, transform);
+
+            //offset to correct position
+            currInstance.transform.Translate(Vector3.up * ScaleRef.transform.localScale.y / 2);
+
+
+            Debug.Log(Vector3.right * (-EQWidth / 2 + EQWidth / NumBins + EQWidth / (NumBins * 2)));
+            currInstance.transform.Translate(Vector3.right * (-EQWidth/2 + EQWidth/NumBins * i + EQWidth/(NumBins*2)));
+
+        }
     }
 }
---
+++ Assets/Scripts/Utility/TransformUtil.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Utility
+{
+    public class TransformUtil
+    {
+        /*
+        public static void ScaleOneDirection(Transform transform, Vector3 scalar)
+        {
+            Vector3 startingScale = transform.localScale;
+            transform.localScale = new Vector3(scalar.x * startingScale.x, scalar.y * startingScale.y, scalar.z * startingScale.z);
+            transform.position = transform.position + ((scalar - startingScale) / 2);
+        }
+        */
+
+        public static void AddScaleOneDirection(Transform transform, Vector3 scalar)
+        {
+            transform.localScale += scalar;
+            transform.position += scalar / 2;
+        }
+    }
+
+}
