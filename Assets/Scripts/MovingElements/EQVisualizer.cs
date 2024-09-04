1377dbd3e042218a60251576d5c70efce9e3215b
Cadence Hagenauer <123522477+cadenceblorbo@users.noreply.github.com>
2024-09-04 10:58:10 -0400

    kind of working eq (needs better scaling)

--- Assets/Scripts/MovingElements/EQVisualizer.cs
+++ Assets/Scripts/MovingElements/EQVisualizer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility;
 
 public class EQVisualizer : MonoBehaviour
 {
@@ -18,10 +19,10 @@ public class EQVisualizer : MonoBehaviour
     int NumBins;
 
     [SerializeField]
-    float BinMin = 0;
+    float BinMax = 5;
 
     [SerializeField]
-    float BinMax = 1;
+    float AmpMult = 10;
 
     [SerializeField]
     [Range(6, 13)]
@@ -31,43 +32,72 @@ public class EQVisualizer : MonoBehaviour
     FFTWindow WindowType = FFTWindow.Rectangular;
 
     float[] Spectrum;
-    float[] Bins;
     GameObject[] BinObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         Spectrum = new float[(int)Mathf.Pow(2, SampleArrayPower)];
-        Bins = new float[NumBins];
         BinObjects = new GameObject[NumBins];
         MakeBins();
     }
 
+    float timer = 0;
+
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        if(timer < 0.05f)
+        {
+            return;
+        }
+        timer = 0;
+
         Audio.GetSpectrumData(Spectrum, 0, WindowType);
+        float[] subSpectrum = new float[64];
+        Array.Copy(Spectrum, subSpectrum, 64);
+        
+
         //Debug.Log(string.Join(",", Spectrum));
+
+        foreach(GameObject binVis in BinObjects)
+        {
+            TransformUtil.AddScaleOneDirection(binVis.transform, -Vector3.up * binVis.transform.localScale.y);
+        }
+
+        for (int i = 0; i < subSpectrum.Length; i++)
+        {
+            int bin = i;
+            TransformUtil.AddScaleOneDirection(BinObjects[bin].transform, AmpMult * subSpectrum[i] * Vector3.up);
+            /*
+                BinObjects[bin].transform.localScale += Vector3.up * Spectrum[i] * AmpMult;
+            if(BinObjects[bin].transform.localScale.y > BinMax)
+            {
+                BinObjects[bin].transform.localScale = new Vector3(BinObjects[bin].transform.localScale.x, BinMax, BinObjects[bin].transform.localScale.z);
+            }
+            */
+        }
     }
 
     void MakeBins()
     {
         float EQWidth = ScaleRef.transform.localScale.x;
-        Vector3 instanceScale = new Vector3(EQWidth / NumBins, 1, ScaleRef.transform.position.z);
-        for (int i = 0; i<Bins.Length; i++)
-        {
-            
-
+        Vector3 instanceScale = new Vector3(EQWidth / NumBins, 0, ScaleRef.transform.localScale.z);
+        for (int i = 0; i<NumBins; i++)
+        { 
             //create block instance
             GameObject currInstance = Instantiate(EQBlock, transform);
 
             //offset to correct position
             currInstance.transform.Translate(Vector3.up * ScaleRef.transform.localScale.y / 2);
-
-
-            Debug.Log(Vector3.right * (-EQWidth / 2 + EQWidth / NumBins + EQWidth / (NumBins * 2)));
             currInstance.transform.Translate(Vector3.right * (-EQWidth/2 + EQWidth/NumBins * i + EQWidth/(NumBins*2)));
 
+            //scale to correct size
+            currInstance.transform.localScale = instanceScale;
+
+            //add to array
+            BinObjects[i] = currInstance;
         }
     }
 }
