31bc52205ddf5c1896659a2270273899f376e6ea
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2024-09-03 20:16:12 -0400

    inv crash fixed

--- Assets/Scripts/InventoryAndItemsScripts/ScriptsForTestingInventory/InputManager.cs
+++ Assets/Scripts/InventoryAndItemsScripts/ScriptsForTestingInventory/InputManager.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 namespace Inventory.Testing
-{ 
-    public class InputManager : MonoBehaviour 
-    { 
-        [SerializeField] private MovementControl movementController; 
-        [SerializeField] private EscMenuController escMenuController; 
-        [SerializeField] private NpcInteractController interactController;
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField]
+        private MovementControl movementController;
+
+        [SerializeField]
+        private EscMenuController escMenuController;
+
+        [SerializeField]
+        private NpcInteractController interactController;
         private InputActions inputScheme;
         private UITestHandler uiTestHandler;
-        
-        private void Awake() 
-        { 
-            inputScheme = new InputActions(); 
+
+        private void Awake()
+        {
+            inputScheme = new InputActions();
             movementController.Initialize(inputScheme.Player.Move);
-            uiTestHandler = new UITestHandler(inputScheme.Player.Menu, inputScheme.Player.Interact, escMenuController, interactController);
+            //uiTestHandler = new UITestHandler(inputScheme.Player.Menu, inputScheme.Player.Interact, escMenuController, interactController);
         }
-    } 
-} 
+    }
+}
