f04760c7ac88c4935ff1330baa3f95af63db404a
Aidan Ormsbee <86251661+wormsbeeeee@users.noreply.github.com>
2024-09-07 15:09:18 -0400

    Changed interacting input system to use paige's technique

--- Assets/Scripts/UIAndMenus/NpcInteractController.cs
+++ Assets/Scripts/UIAndMenus/NpcInteractController.cs
@@ -2,27 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class NpcInteractController : MonoBehaviour
 {
     string postInteractText = "Dialogue";
     bool playerDetection = false;
     private Transform npc;
-    public GameObject popup;  // this isn't being used? I don't think we need it
+    // public GameObject popup;  // this isn't being used? I don't think we need it
     public GameObject canvas;
+
+    [SerializeField]
+    [Tooltip("The keybinds that control lateral character movement. To restrict character movement to a single axis, unbind the directions you don't want accessible.")]
+    InputAction Interact;
 
     private void Start()
     {
         npc = transform.parent;
     }
 
-    public void Interact()
+    private void OnEnable()
     {
-        Debug.Log("Test");
+        Interact.performed += DoInteraction;
+        Interact.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Interact.performed -= DoInteraction;
+        Interact.Disable();
+    }
+
+    public void DoInteraction(CallbackContext c)
+    {
         if(playerDetection)
         {
             Debug.Log("NPC Interacted with player!");
-            canvas.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = postInteractText;
+            // canvas.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = postInteractText;
         }
     }
 
