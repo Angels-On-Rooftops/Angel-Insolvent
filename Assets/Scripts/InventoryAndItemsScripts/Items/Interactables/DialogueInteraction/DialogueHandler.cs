c453014547fbcca4bca2fad0b07cb9394780d2d3
Saran Lendzian <lendzian.1@osu.edu>
2024-09-07 21:58:51 -0400

    IT WORKS!!! (DialogueHandler connects nodes to ui output)

--- Assets/Scripts/InventoryAndItemsScripts/Items/Interactables/DialogueInteraction/DialogueHandler.cs
+++ Assets/Scripts/InventoryAndItemsScripts/Items/Interactables/DialogueInteraction/DialogueHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Linq;
+using UnityEngine.EventSystems;
 
 namespace Items.Interactables.Dialogue
 {
@@ -11,11 +12,15 @@ namespace Items.Interactables.Dialogue
     {
         [SerializeField] private DialogueContainer dialogueContainer;
         [SerializeField] private TMP_Text currentDialogueText;
-        [SerializeField] private Button button1;
-        [SerializeField] private Button button2;
+        [SerializeField] private GameObject buttonPrefab;
+        [SerializeField] private Vector2 firstButtonPosition;
+        [SerializeField] private Vector2 buttonDisplacement;
+        [SerializeField] private GameObject parentCanvas;
 
         //private NodeLinkData currentNodeLinkData;
         private DialogueNodeData currentDialogueNodeData;
+
+        private List<GameObject> currentButtons = new List<GameObject>();
 
         void Awake()
         {
@@ -25,17 +30,54 @@ namespace Items.Interactables.Dialogue
             //Get the first dialogue node by following where the link goes
             this.currentDialogueNodeData = this.dialogueContainer.DialogueNodeData.First(node => node.NodeGuID == firstNodeLink.TargetNodeGuID);
 
-            this.currentDialogueText.text = this.currentDialogueNodeData.DialogueText;
-
-
+            getCurrentScreen();
 
             /*List<NodeLinkData> childNodes = this.dialogueContainer.NodeLinks.Where(x => x.BaseNodeGuID == Nodes[i].GUID).ToList();
             this.button1.GetComponent<TMP_Text>().text = this.currentDialogueNodeData.DialogueText;*/
         }
 
-        private void getText()
+        private void getCurrentScreen()
         {
+            //Clear previous buttons
+            for (int i = this.currentButtons.Count - 1; i >= 0; i--)
+            {
+                Destroy(this.currentButtons[i]);
+            }
+            
+            this.currentDialogueText.text = this.currentDialogueNodeData.DialogueText;
 
+            List<NodeLinkData> childNodes = 
+                this.dialogueContainer.NodeLinks.Where(node => node.BaseNodeGuID == this.currentDialogueNodeData.NodeGuID).ToList();
+
+            Vector2 buttonPosition = this.firstButtonPosition;
+            foreach (NodeLinkData childNode in childNodes)
+            {
+                CreateButton(buttonPosition, childNode);
+
+                buttonPosition += this.buttonDisplacement;
+            }
+        }
+
+        private void CreateButton(Vector2 buttonPosition, NodeLinkData childNode)
+        {
+            GameObject newButton = Instantiate(this.buttonPrefab, buttonPosition, Quaternion.identity, this.parentCanvas.transform);
+            
+            Button newButtonButton = newButton.GetComponentInChildren<Button>();
+            newButtonButton.onClick.AddListener(delegate {OnButtonPress(newButton);});
+
+            newButton.GetComponentInChildren<TMP_Text>().text = childNode.PortName;
+
+            this.currentButtons.Add(newButton);
+        }
+
+        void OnButtonPress(GameObject button)
+        {
+            //Update current node with node button port goes to
+            string buttonPortName = button.GetComponentInChildren<TMP_Text>().text;
+            NodeLinkData nextNodeLinkData = this.dialogueContainer.NodeLinks.First(node => node.PortName == buttonPortName);
+            this.currentDialogueNodeData = this.dialogueContainer.DialogueNodeData.First(node => node.NodeGuID == nextNodeLinkData.TargetNodeGuID);
+
+            getCurrentScreen();
         }
     }
 }
