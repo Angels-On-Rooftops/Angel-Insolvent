367cff54aa9e4baf883af9d0364da8b16793a816
jidaprano <117032914+jidaprano@users.noreply.github.com>
2024-09-10 17:01:26 -0400

    Game state manager (#7)
    * fix input system
    
    * prefab error checking in SettingsController
    
    * fix error checking
    
    * start setting up game state manager
    
    * refine gamestatemanager
    
    * pre-PR finished game state manager
    
    * fix PR comments
    
    * fix quit button in esc menu

--- Assets/Scripts/InventoryAndItemsScripts/ScriptsForTestingInventory/InputManager.cs
+++ Assets/Scripts/InventoryAndItemsScripts/ScriptsForTestingInventory/InputManager.cs
@@ -8,16 +8,12 @@ namespace Inventory.Testing
     public class InputManager : MonoBehaviour 
     { 
         [SerializeField] private MovementControl movementController; 
-        [SerializeField] private EscMenuController escMenuController; 
-        [SerializeField] private NpcInteractController interactController;
         private InputActions inputScheme;
-        private UITestHandler uiTestHandler;
         
         private void Awake() 
         { 
             inputScheme = new InputActions(); 
             movementController.Initialize(inputScheme.Player.Move);
-            uiTestHandler = new UITestHandler(inputScheme.Player.Menu, inputScheme.Player.Interact, escMenuController, interactController);
         }
     } 
 } 
