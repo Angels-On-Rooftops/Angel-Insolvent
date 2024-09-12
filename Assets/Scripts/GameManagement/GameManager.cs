fae9a3559cb340e2bb5ea50bbb7f5cabdbb1d04f
Saran Lendzian <lendzian.1@osu.edu>
2024-09-12 13:03:23 -0400

    Set up URP, and Imported in Starter Assets to help with Grey-Boxing

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -6,15 +6,22 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    //Eventually remove this check--Saran added it to make running individual scenes temporarily easier
+    //Whoever is reviewing this pull request, if I forget to put this back to true, please leave a comment
+    private static bool runGameManager = false;
+    
     public static GameObject escMenu;
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     static void OnBeforeSplashScreen()
     {
-        GameObject gameObject = Instantiate(new GameObject("GameManager"));
-        DontDestroyOnLoad(gameObject);
-        gameObject.AddComponent<GameStateManager>();
-        escMenu = Instantiate(Resources.Load("Prefabs/UI/EscMenuPrefab")) as GameObject;
-        escMenu.gameObject.transform.parent = gameObject.transform;
+        if (runGameManager)
+        {
+            GameObject gameObject = Instantiate(new GameObject("GameManager"));
+            DontDestroyOnLoad(gameObject);
+            gameObject.AddComponent<GameStateManager>();
+            escMenu = Instantiate(Resources.Load("Prefabs/UI/EscMenuPrefab")) as GameObject;
+            escMenu.gameObject.transform.parent = gameObject.transform;
+        } 
     }
 }
