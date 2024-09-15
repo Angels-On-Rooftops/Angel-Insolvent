29238347dc94aa6e5063dccdf03d3626e9fe38d9
Saran Lendzian <lendzian.1@osu.edu>
2024-09-15 15:03:31 -0400

    removed my temp fix to get the game manager not to run when I was testing since main now has something else to do that now

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -6,22 +6,15 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    //Eventually remove this check--Saran added it to make running individual scenes temporarily easier
-    //Whoever is reviewing this pull request, if I forget to put this back to true, please leave a comment
-    private static bool runGameManager = false;
-    
     public static GameObject escMenu;
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     static void OnBeforeSplashScreen()
     {
-        if (runGameManager)
-        {
-            GameObject gameObject = Instantiate(new GameObject("GameManager"));
-            DontDestroyOnLoad(gameObject);
-            gameObject.AddComponent<GameStateManager>();
-            escMenu = Instantiate(Resources.Load("Prefabs/UI/EscMenuPrefab")) as GameObject;
-            escMenu.gameObject.transform.parent = gameObject.transform;
-        } 
+        GameObject gameObject = Instantiate(new GameObject("GameManager"));
+        DontDestroyOnLoad(gameObject);
+        gameObject.AddComponent<GameStateManager>();
+        escMenu = Instantiate(Resources.Load("Prefabs/UI/EscMenuPrefab")) as GameObject;
+        escMenu.gameObject.transform.parent = gameObject.transform;
     }
 }
