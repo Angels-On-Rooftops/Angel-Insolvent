b1044ac50fa9c18e8b9d7b449c4c1949375a629b
Cadence Hagenauer <123522477+cadenceblorbo@users.noreply.github.com>
2024-09-25 17:23:01 -0400

    shitty ass specular

--- Assets/Resources/Materials/shader/ToonLighting.hlsl
+++ Assets/Resources/Materials/shader/ToonLighting.hlsl
@@ -6,6 +6,7 @@ struct ToonLightingParams
     float3 albedo;
     float3 normal;
     float3 viewDir;
+    float smoothness;
 };
 
 #ifndef SHADERGRAPH_PREVIEW
@@ -13,14 +14,16 @@ float3 CalculateDiffuse(ToonLightingParams params, Light light)
 {
     float diffuseScalar = dot(params.normal, light.direction);
     
-    return params.albedo * light.color * diffuseScalar;
+    return light.color * diffuseScalar;
 
 }
 
 float3 CalculateSpecular(ToonLightingParams params, Light light)
 {
-    float3 reflectionVector = reflect(light.direction, )
-    float3 specular = 
+    float3 reflectionVector = -reflect(light.direction, params.normal);
+    float dotProduct = dot(reflectionVector, params.viewDir);
+    float3 specular = pow(dotProduct, params.smoothness);
+    return specular;
 }
 #endif
 
@@ -33,18 +36,21 @@ float3 CalculateLighting(ToonLightingParams params)
     float3 result = 0;
     
     result += CalculateDiffuse(params, GetMainLight());
+    //result += CalculateSpecular(params, GetMainLight());
+    result *= params.albedo;
     
     return result;
     #endif
 }
 
-void ToonLighting_float(float3 Albedo, float3 Normal, float3 ViewDir, out float3 Color)
-{
+void ToonLighting_float(float3 Albedo, float3 Normal, float3 ViewDir, float3 Smoothness, out float3 Color)
+{ 
     ToonLightingParams params;
     
     params.albedo = Albedo;
     params.normal = Normal;
     params.viewDir = ViewDir;
+    params.smoothness = Smoothness;
     
     Color = CalculateLighting(params);
 }
