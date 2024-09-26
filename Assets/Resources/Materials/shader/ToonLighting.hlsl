7ab724dffcc56f1f525a97e5c029cc7c2056f6de
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2024-09-25 20:32:26 -0400

    add shadow support

--- Assets/Resources/Materials/shader/ToonLighting.hlsl
+++ Assets/Resources/Materials/shader/ToonLighting.hlsl
@@ -3,10 +3,15 @@
 
 struct ToonLightingParams
 {
+    //surface
     float3 albedo;
     float3 normal;
-    float3 viewDir;
     float smoothness;
+    
+    //relational info
+    float3 viewDir;
+    float3 fragWorldPos;
+    float4 shadowCoordinate;
 };
 
 float GetSmoothnessPower(float smoothness)
@@ -27,7 +32,7 @@ float3 CalculateOneLight(ToonLightingParams params, Light light)
     float3 specular = smoothedSpecularDot * diffuse;
     
     //contribution from light
-    return params.albedo * light.color * (diffuse + specular * diffuse);
+    return params.albedo * light.color * light.shadowAttenuation * (diffuse + specular * diffuse);
 
 }
 #endif
@@ -36,30 +41,56 @@ float3 CalculateLighting(ToonLightingParams params)
 {
     #ifdef SHADERGRAPH_PREVIEW
     //approximate lighting for node preview
-    float3 lightDir = float3(0.5,0.5,0);
+        float3 lightDir = float3(0.5,0.5,0);
     
-    float3 diffuse = dot(lightDir, params.normal);
+        float3 diffuse = dot(lightDir, params.normal);
     
-    float3 middle = normalize(params.viewDir + lightDir);
-    float3 specularDotProduct = saturate(dot(middle, params.normal));
-    float3 smoothedSpecularDot = pow(specularDotProduct, GetSmoothnessPower(params.smoothness));
-    float3 specular = smoothedSpecularDot * diffuse;
+        float3 middle = normalize(params.viewDir + lightDir);
+        float3 specularDotProduct = saturate(dot(middle, params.normal));
+        float3 smoothedSpecularDot = pow(specularDotProduct, GetSmoothnessPower(params.smoothness));
+        float3 specular = smoothedSpecularDot * diffuse;
     
-    return params.albedo * (diffuse+specular);
+        return params.albedo * (diffuse+specular);
     #else
     
-    return CalculateOneLight(params, GetMainLight());
+        return CalculateOneLight(params, GetMainLight(params.shadowCoordinate, params.fragWorldPos, 1));
     #endif
 }
 
-void ToonLighting_float(float3 Albedo, float3 Normal, float3 ViewDir, float3 Smoothness, out float3 Color)
-{ 
+float4 GetShadowCoordinate(float3 pos)
+{
+    //don't inclide shadows in shadergraph preview
+    #ifdef SHADERGRAPH_PREVIEW
+        return 0;
+    #else
+        //get coords based on if using screen space deferred cascaded shadows
+        #if SHADOWS_SCREEN
+            float4 clipSpacePos = TransformWorldToHClip(pos);
+            return ComputeScreenPos(clipSpacePos);
+        #else
+            return TransformWorldToShadowCoord(pos);
+        #endif
+    #endif
+   
+}
+
+//This function exists to allow this script to be used as a node in Unity's ShaderGraph
+void ToonLighting_float(
+    float3 Albedo, 
+    float3 Normal, 
+    float3 ViewDir, 
+    float Smoothness, 
+    float3 WorldPos,
+    out float3 Color
+){ 
     ToonLightingParams params;
     
     params.albedo = Albedo;
     params.normal = normalize(Normal);
     params.viewDir = normalize(ViewDir);
     params.smoothness = Smoothness;
+    params.fragWorldPos = WorldPos;
+    params.shadowCoordinate = GetShadowCoordinate(WorldPos);
     
     Color = CalculateLighting(params);
 }
