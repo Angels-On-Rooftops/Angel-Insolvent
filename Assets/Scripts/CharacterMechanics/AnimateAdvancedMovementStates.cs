bae91a1c372bada9ac3e5b8ce151802de339f7d2
Paige <33643911+prepsure@users.noreply.github.com>
2024-10-01 00:01:16 -0400

    animation thingies

---
+++ Assets/Scripts/CharacterMechanics/AnimateAdvancedMovementStates.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimateAdvancedMovementStates : MonoBehaviour
+{
+    [SerializeField]
+    CharacterMovement BasicMovementController;
+
+    [SerializeField]
+    AdvancedMovement AdvancedMovementController;
+
+    Animator Animator => GetComponent<Animator>();
+
+    readonly Maid maid = new();
+
+
+
+    void OnEnable()
+    {
+        maid.GiveEvent(BasicMovementController, "StartedWalking", () => SetWalk(true));
+        maid.GiveEvent(BasicMovementController, "StoppedWalking", () => SetWalk(false));
+
+        //maid.GiveEvent(BasicMovementController, "Jumped", (int _) => SetTrigger("Jump"));
+        //maid.GiveEvent(BasicMovementController, "Falling", () => SetTrigger("Fall"));
+        maid.GiveEvent(BasicMovementController, "Landed", () => SetTrigger("Land"));
+
+        Dictionary<AdvancedMovementState, Action> StateChanges = new()
+        {
+            { AdvancedMovementState.Rolling, () => {
+                Animator.ResetTrigger("RollStop");
+                SetTrigger("RollStart");
+            } },
+            { AdvancedMovementState.LongJumping, () => SetTrigger("LongJump") },
+            { AdvancedMovementState.HighJumping, () => SetTrigger("HighJump") },
+            { AdvancedMovementState.Gliding, () => SetTrigger("Glide") },
+            { AdvancedMovementState.Plunging, () => SetTrigger("Plunge") },
+        };
+
+        maid.GiveEvent(
+            AdvancedMovementController, 
+            "StateChanged", 
+            (AdvancedMovementState last, AdvancedMovementState next) =>
+            {
+                if (StateChanges.ContainsKey(next))
+                {
+                    StateChanges[next]();
+                }
+            }
+        );
+
+        maid.GiveEvent(
+            AdvancedMovementController, 
+            "StateChanged", 
+            (AdvancedMovementState last, AdvancedMovementState next) =>
+            {
+                if (last == AdvancedMovementState.Rolling)
+                {
+                    SetTrigger("RollStop");
+                }
+            }
+        );
+    }
+
+    void OnDisable()
+    {
+        maid.Cleanup();
+    }
+
+    void SetTrigger(string state)
+    {
+        Animator.SetTrigger(state);
+    }
+
+    void SetWalk(bool state)
+    {
+        Animator.SetBool("IsMoving", state);
+    }
+}
