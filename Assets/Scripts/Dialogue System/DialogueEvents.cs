03d85d7263f0d9426116b71fdd240e3f4b1105a5
Paige <33643911+prepsure@users.noreply.github.com>
2024-10-10 19:58:32 -0400

    dialogue events get triggered by reflection api

--- Assets/Scripts/Dialogue System/DialogueEvents.cs
+++ Assets/Scripts/Dialogue System/DialogueEvents.cs
@@ -1,19 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using UnityEngine.Assertions;
 
 namespace Assets.Scripts.Dialogue_System
 {
-    public static class DialogueEvents
+    public class DialogueEvents
     {
         public static event Action RancidVibes;
 
+        #region Singleton BS
+        private static DialogueEvents instance = null;
+        private static readonly object instanceLock = new object(); //thread-safe for co-routines
+
+        public static DialogueEvents Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    instance ??= new DialogueEvents();
+                    return instance;
+                }
+            }
+        }
+        #endregion
+
         public static void FireEvent(string eventName)
         {
-            // TODO
-            //this.GetType().GetEvent(eventName).
+            Assert.IsTrue(
+                Instance.GetType().GetEvent(eventName, BindingFlags.Static) is not null,
+                $"{eventName} is not a static dialogue event."
+            );
+
+            // TODO check that this works
+            Instance
+                .GetType()
+                .GetEvent(eventName, BindingFlags.Static)
+                .GetRaiseMethod()
+                ?.Invoke(null, null);
         }
     }
 }
