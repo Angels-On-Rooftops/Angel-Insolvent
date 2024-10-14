0d4d31a060bdf68de83f07196841481ad48ac3bb
Paige <33643911+prepsure@users.noreply.github.com>
2024-10-14 02:26:22 -0400

    add coda to character list and add another example dialogue

--- Assets/Scripts/DialogueSystem/Characters.cs
+++ Assets/Scripts/DialogueSystem/Characters.cs
@@ -13,5 +13,6 @@ namespace Assets.Scripts.DialogueSystem
             new("Overhead Speaker", new Color(255, 255, 255));
         public static NarrativeCharacter Opus = new("Opus", new Color(255, 255, 255));
         public static NarrativeCharacter Coda = new("Coda", new Color(255, 255, 0));
+        public static NarrativeCharacter Clef = new("Coda", new Color(0, 255, 0));
     }
 }
