a3021866fb427004c0a4c1043f3dcd423b144f14
Paige <33643911+prepsure@users.noreply.github.com>
2024-10-19 11:35:24 -0400

    fix animations

--- Assets/Scripts/CharacterMechanics/AnimateAdvancedMovementStates.cs
+++ Assets/Scripts/CharacterMechanics/AnimateAdvancedMovementStates.cs
@@ -14,84 +14,49 @@ public class AnimateAdvancedMovementStates : MonoBehaviour
 
     Animator Animator => GetComponent<Animator>();
 
-    readonly Maid maid = new();
-
-
-
-    void OnEnable()
-    {
-        maid.GiveEvent(BasicMovementController, "StartedWalking", () => SetWalk(true));
-        maid.GiveEvent(BasicMovementController, "StoppedWalking", () => SetWalk(false));
-
-        maid.GiveEvent(BasicMovementController, "Jumped", (int _) => SetTrigger("Jump"));
-        maid.GiveEvent(BasicMovementController, "Falling", () => SetTrigger("Fall"));
-
-        maid.GiveEvent(BasicMovementController, "Landed", () => ResetTrigger("Fall"));
-        maid.GiveEvent(BasicMovementController, "Landed", () => ResetTrigger("Jump"));
-        maid.GiveEvent(BasicMovementController, "Landed", () => SetTrigger("Land"));
-
-        Dictionary<AdvancedMovementState, Action> StateChanges = new()
+    readonly Dictionary<string, AdvancedMovementState> advancedStateBindings =
+        new()
         {
-            { AdvancedMovementState.Rolling, () => {
-                Animator.ResetTrigger("RollStop");
-                SetTrigger("RollStart");
-            } },
-            { AdvancedMovementState.LongJumping, () => SetTrigger("LongJump") },
-            { AdvancedMovementState.HighJumping, () => SetTrigger("HighJump") },
-            { AdvancedMovementState.Gliding, () => SetTrigger("Glide") },
-            { AdvancedMovementState.Plunging, () => SetTrigger("Plunge") },
+            { "Rolling", AdvancedMovementState.Rolling },
+            { "HighJumping", AdvancedMovementState.HighJumping },
+            { "LongJumping", AdvancedMovementState.LongJumping },
+            { "Plunging", AdvancedMovementState.Plunging },
+            { "Gliding", AdvancedMovementState.Gliding },
         };
 
-        maid.GiveEvent(
-            AdvancedMovementController, 
-            "StateChanged", 
-            (AdvancedMovementState last, AdvancedMovementState next) =>
-            {
-                if (StateChanges.ContainsKey(next))
-                {
-                    StateChanges[next]();
-                    return;
-                }
-                
-            }
-        );
-
-        maid.GiveEvent(
-            AdvancedMovementController, 
-            "StateChanged", 
-            (AdvancedMovementState last, AdvancedMovementState next) =>
-            {
-                Debug.Log($"{last} {next}");
-                if (last == AdvancedMovementState.Rolling)
-                {
-                    SetTrigger("RollStop");
-                }
-            }
-        );
-    }
-
-    void OnDisable()
-    {
-        maid.Cleanup();
-    }
-
-    void SetTrigger(string state)
-    {
-        Animator.SetTrigger(state);
-    }
-
-    void ResetTrigger(string state)
-    {
-        Animator.ResetTrigger(state);
-    }
-
-    void SetWalk(bool state)
-    {
-        Animator.SetBool("IsMoving", state);
-    }
+    readonly Dictionary<string, VerticalMovementState> verticalStateBindings =
+        new()
+        {
+            { "Jumping", VerticalMovementState.Jumping },
+            { "Falling", VerticalMovementState.Falling },
+            { "Grounded", VerticalMovementState.Grounded },
+        };
 
     private void Update()
     {
-        Animator.SetFloat("RunSpeed", (BasicMovementController.WalkSpeed * BasicMovementController.MovementDirection).magnitude/16);
+        bool inAdvancedState = false;
+
+        foreach ((string animatorBool, AdvancedMovementState state) in advancedStateBindings)
+        {
+            bool shouldSet = AdvancedMovementController.CurrentState == state;
+            Animator.SetBool(animatorBool, shouldSet);
+            inAdvancedState |= shouldSet;
+        }
+
+        Animator.SetBool("InAdvancedState", inAdvancedState);
+
+        foreach ((string animatorBool, VerticalMovementState state) in verticalStateBindings)
+        {
+            Animator.SetBool(animatorBool, BasicMovementController.VerticalState == state);
+        }
+
+        Animator.SetBool("IsMoving", BasicMovementController.MovementDirection.magnitude != 0);
+
+        Animator.SetFloat(
+            "RunSpeed",
+            (
+                BasicMovementController.WalkSpeed * BasicMovementController.MovementDirection
+            ).magnitude / 16
+        );
     }
 }
