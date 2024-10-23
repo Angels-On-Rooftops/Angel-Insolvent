257215fb0ca74200a63eead0761966aa643cba2e
Saran Lendzian <lendzian.1@osu.edu>
2024-10-23 00:12:07 -0400

    Kind of works (still need to fix a few bugs)

--- Assets/Scripts/MovingElements/ParticleHitbox.cs
+++ Assets/Scripts/MovingElements/ParticleHitbox.cs
@@ -14,7 +14,26 @@ public class ParticleHitbox : MonoBehaviour
     [Space]
     [SerializeField] private Notes indicatorBeatNote;
     [SerializeField] private int indicatorBeatOctave;
+    [Space]
+    [SerializeField] [Tooltip("Leave empty if a hitbox is not desired")]
+    private BoxCollider hitbox;
+    [SerializeField][Tooltip("z dimension of the hitbox right before dissipating (minimum z size)")]
+    private float endHitboxDepth;
+    [SerializeField][Tooltip("Distance of particles/hitbox from initial point before dissipating (maximum distance between the initial position of the center of the hitbox and the final position of the center of the hitbox)")]
+    private float distance;
+    [SerializeField] private Vector3 hitboxVelocity;
+    [SerializeField] private float hitboxShrinkSpeed;
+    [SerializeField][Tooltip("Time after chart note ends before disappears (may need to be > 0 to prevent bugs)")]
+    private float timeBeforeDisappear = 0.25f;
+    [Space]
+    [SerializeField][Tooltip("Because several settings can effect particle distance/time, adjusting particle settings through script is optional in case it does not work with your specific particle settings")]
+    private bool adjustParticleSettings;
 
+    private Vector3 initialHitboxPosition;
+    private float distanceTraveled = 0;
+
+    private bool stopWasCalled = false;
+    
     private Action unsubPlay;
     private Action unsubStop;
 
@@ -22,12 +41,19 @@ public class ParticleHitbox : MonoBehaviour
     void Start()
     {
         this.particleSys.Stop();
+        this.initialHitboxPosition = this.hitbox.transform.position;
+        this.hitbox.transform.localScale = new Vector3(this.hitbox.transform.localScale.x, this.hitbox.transform.localScale.y, 0);
+
+        if (this.adjustParticleSettings)
+        {
+            SetParticleSettings();
+        }
     }
 
     private void OnEnable()
     {
         unsubPlay = midiEventSys.Subscribe(PlayParticles, note, octave);
-        unsubStop = midiEventSys.Subscribe(StopParticles, note, octave, Midi2Event.SubType.End);
+        unsubStop = midiEventSys.Subscribe(StopParticles, note, octave, Midi2Event.SubType.Stop);
     }
 
     private void OnDisable()
@@ -42,30 +68,91 @@ public class ParticleHitbox : MonoBehaviour
         
     }
 
-    int i = 0;
-    bool playing = false;
+    private void SetParticleSettings()
+    {
+        //TODO
+    }
+
     void PlayParticles()
     {
-        /*i++;
-        if (i >= 10)
-        {
-            if (playing)
-            {
-                this.particleSys.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-                playing = false;
-            }
-            {
-                this.particleSys.Play();
-                playing = true;
-            }
-            i = 0;
-        }*/
         this.particleSys.Play();
+
+        StartCoroutine(MoveHitboxCoRoutine());
+    }
+
+    IEnumerator MoveHitboxCoRoutine()
+    {
+        while (ShouldMoveHitbox())
+        {
+            MoveHitbox();
+            yield return null;
+        }
+    }
+
+    private void MoveHitbox()
+    {
+        if (this.distanceTraveled < this.distance)
+        {
+            //Move hitbox center
+            Vector3 movementVector = this.hitboxVelocity * Time.deltaTime;
+            this.hitbox.transform.Translate(movementVector);
+
+            //Increase hitbox size based on translation
+            float distanceMovedThisFrame = movementVector.magnitude;
+            this.hitbox.transform.localScale = new Vector3(this.hitbox.transform.localScale.x, this.hitbox.transform.localScale.y,
+                this.hitbox.transform.localScale.z + distanceMovedThisFrame * 2);
+
+            //Update this.distanceTraveled
+            this.distanceTraveled += distanceMovedThisFrame;
+        }
+        else if (this.hitbox.size.z > this.endHitboxDepth)
+        {
+            //Finished moving the hitbox, so start shrinking the hitbox until it's size.z is this.endHitboxDepth
+            float sizeDecreaseFactor = 1 - this.hitboxShrinkSpeed * Time.deltaTime;
+            this.hitbox.transform.localScale = new Vector3(this.hitbox.transform.localScale.x, this.hitbox.transform.localScale.y,
+                this.hitbox.transform.localScale.z * sizeDecreaseFactor);
+        }
+    }
+
+    private bool ShouldMoveHitbox()
+    {
+        //Check if StopParticles was called (should not change the hitbox anymore if so)
+        if (this.stopWasCalled)
+        {
+            return false;
+        }
+
+        return (this.distanceTraveled < this.distance)
+                    || (this.hitbox.transform.localScale.z > this.endHitboxDepth);
     }
 
     void StopParticles()
     {
-        //this.particleSys.Stop();
-        this.particleSys.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        //this.stopWasCalled = true;
+
+        //StartCoroutine(Disappear());
+    }
+
+    IEnumerator Disappear()
+    {
+        this.particleSys.Stop();
+
+        yield return new WaitForSeconds(this.timeBeforeDisappear);
+
+        //this.particleSys.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        Reset();
+
+        
+    }
+
+    private void Reset()
+    {
+        //Reset Hitbox
+        this.hitbox.transform.position = this.initialHitboxPosition;
+        this.hitbox.transform.localScale = new Vector3(this.hitbox.transform.localScale.x, this.hitbox.transform.localScale.y, 0);
+
+        this.distanceTraveled = 0;
+
+        this.stopWasCalled = false;
     }
 }
