790f0e1e6f4c843f19ba52a13b6265e8fd907d54
JayDee Thomas <93546044+JayDeeThomas@users.noreply.github.com>
2024-10-23 17:46:21 -0400

    adding stuff to pathfinding

--- Assets/Scripts/PathfindingSystem/NodeMarker.cs
+++ Assets/Scripts/PathfindingSystem/NodeMarker.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NodeMarker : MonoBehaviour
-{
+public class NodeMarker : MonoBehaviour {
     //all the nodes that can be travled to
-    public List<NodeMarker> nextNode;
+    [SerializeField] List<NodeMarker> nextNode; //list of avaliable node
+    public List<NodeMarker> NextNode { get { return nextNode; } }
+    [SerializeField] string objTag; //objects to check for collison
+
+    private bool isOccupied = false;
+    public bool Occupied { get { return isOccupied; } }
+
 
     //visualize all of the nodes connected
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos() {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, 0.25f);
+        Gizmos.DrawWireSphere(this.transform.position, 0.25f);
 
-        foreach (var node in nextNode)
-        {
-            Gizmos.color = Color.white;
-            Gizmos.DrawLine(transform.position, node.gameObject.transform.position);
+        if (nextNode != null && nextNode.Count > 0) {
+            foreach (var node in nextNode) {
+                Gizmos.color = Color.white;
+                Gizmos.DrawLine(this.transform.position, node.gameObject.transform.position);
+                }
+            }
+        }
+
+    private void OnTriggerEnter(Collider other) {
+        if (objTag != null && other.gameObject.CompareTag(objTag)) {
+            isOccupied = true;
+            }
+        }
+    private void OnTriggerExit(Collider other) {
+        if (objTag != null && other.gameObject.CompareTag(objTag)) {
+            isOccupied = false;
+            }
         }
     }
-}
