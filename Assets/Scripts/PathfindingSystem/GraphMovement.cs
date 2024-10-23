0d99c8170d83702518633b21cfb9c9b638277209
JayDee Thomas <93546044+JayDeeThomas@users.noreply.github.com>
2024-10-23 17:39:20 -0400

    pathfinding stuff

--- Assets/Scripts/PathfindingSystem/GraphMovement.cs
+++ Assets/Scripts/PathfindingSystem/GraphMovement.cs
@@ -1,92 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MIDI2EventSystem;
+using System;
+using Utility;
+using Random = UnityEngine.Random;
+public class GraphMovement : MonoBehaviour {
+    [SerializeField] Pathfinding path; //pathfinding
+    [SerializeField] MarkGraph map; //all of the nodes
+    float speed; //speed of object
 
-// TODO make wording more generic, not just going to be used for cars
-public class GraphMovement : MonoBehaviour
-{
-    [SerializeField]
-    MarkGraph map; //all of the nodes
+    private List<NodeMarker> pathway; //pathway that objeect follows
+    private int position = 0; //position in pathway
+    private NodeMarker node1; //start node
+    private NodeMarker node2; //end node
 
-    public float speed = 5.0f;
 
-    [Space(20)]
-    public List<NodeMarker> pathway; //pathway that car follows
-    public NodeMarker node1;
-    public NodeMarker node2;
-    public int position = 0;
+    [SerializeField] MIDI2EventUnity handler;
+    [SerializeField] int BeatPeriod;
+    [SerializeField] float speedMod = 1.0f;
 
-    Pathfinding path => GetComponent<Pathfinding>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Awake() {
         //inital Pathway
         node1 = map.map[Random.Range(0, map.map.Count)];
         node2 = map.map[Random.Range(0, map.map.Count)];
 
-        pathway = path.FindPath(node1, node2);
+        pathway = path.findPath(node1, node2);
         this.transform.position = pathway[0].transform.position;
-    }
+
+        }
 
     // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
-
-    private void OnDrawGizmos()
-    {
-        foreach (NodeMarker node in pathway)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(node.transform.position, 0.25f);
+    void Update() {
+        if (!handler.IsPlaying) {
+            return;
+            }
+        speed = speedMod * handler.BeatPerSec * 1 / BeatPeriod * Time.deltaTime;
+        moveCar();
         }
-        if (pathway.Count >= 2)
-        {
-            for (int i = 0; i < pathway.Count - 1; i++)
-            {
-                Gizmos.color = Color.red;
-                Gizmos.DrawLine(pathway[i].transform.position, pathway[i + 1].transform.position);
+
+
+    public void moveCar() {
+        if (this.transform.position != pathway[position].transform.position) {
+            this.transform.LookAt(pathway[position].transform.position);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pathway[position].transform.position, speed);
+            }
+        else {
+            changeGoal();
             }
         }
-    }
 
-    public void Move()
-    {
-        //move the car to current node
-        if (this.transform.position != pathway[position].transform.position)
-        {
-            this.transform.LookAt(pathway[position].transform.position);
-            this.transform.position = Vector3.MoveTowards(
-                this.transform.position,
-                pathway[position].transform.position,
-                speed * Time.deltaTime
-            );
-        }
-        //increment to next node
-        else if (position < pathway.Count - 1)
-        {
-            position++;
-        }
-        else
-        {
-            //generate new pathway after end is reached
-            position = 0;
-            resetPathway();
-        }
-    }
+    public void changeGoal() {
+        if (this.transform.position == pathway[position].transform.position) {
+            //increment to next point
+            if (position < pathway.Count - 1) {
+                if (!pathway[position + 1].Occupied) {
+                    position++;
+                    }
 
-    //function to get new pathway once destination is reached
-    //new path always needs to start with the old end and get new end
-    public void resetPathway()
-    {
+                }
+            else {
+                //generate new pathway after end is reached
+                position = 0;
+                resetPathway();
+                }
+            }
+
+        }
+    public void resetPathway() {
         pathway.Clear(); //erase old pathway
         node1 = node2; //swith end to be start
-        node2 = map.map[Random.Range(0, map.map.Count)]; //new start
-        pathway = path.FindPath(node1, node2); //calculate path for new start and end
+        node2 = map.map[Random.Range(0, map.map.Count)]; //new end
+        pathway = path.findPath(node1, node2); //calculate path for new start and end
+        }
     }
-
-    //stop cars from hitting each other
-    //make one of the cars generate a new pathway || stop one of the cars until the other is out of range
-}
