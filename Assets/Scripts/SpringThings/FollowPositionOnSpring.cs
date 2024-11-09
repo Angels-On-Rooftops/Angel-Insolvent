2330739e14cd3da6abf11ba49e445b690b80bc7b
Sophie Young <flwrstems@proton.me>
2024-11-08 22:01:43 -0500

    add debug statements

--- Assets/Scripts/SpringThings/FollowPositionOnSpring.cs
+++ Assets/Scripts/SpringThings/FollowPositionOnSpring.cs
@@ -21,26 +21,43 @@ public class FollowPositionOnSpring : MonoBehaviour
         {
             return;
         }
-
-        transform.position = new Vector3(
-            Mathf.SmoothDamp(
+        
+        float newX = Mathf.SmoothDamp(
                 transform.position.x,
                 ToFollow.position.x,
                 ref xVelocity,
                 PositionSmoothing.x * Time.deltaTime
-            ),
-            Mathf.SmoothDamp(
+        ); 
+        float newY = Mathf.SmoothDamp(
                 transform.position.y,
                 ToFollow.position.y,
                 ref yVelocity,
                 PositionSmoothing.y * Time.deltaTime
-            ),
+        );
+        float newZ = 
             Mathf.SmoothDamp(
                 transform.position.z,
                 ToFollow.position.z,
                 ref zVelocity,
                 PositionSmoothing.z * Time.deltaTime
-            )
         );
+
+        Vector3 newPos = new Vector3(newX, newY, newZ);
+
+        bool didHit = Physics.Raycast(
+            ToFollow.position,
+            newPos - ToFollow.position,
+            out RaycastHit hit,
+            (newPos - ToFollow.position).magnitude,
+            ControlConstants.RAYCAST_MASK,
+            QueryTriggerInteraction.Ignore
+        );
+
+        if(didHit) {
+            Debug.Log("didHit");
+            
+            //transform.position = ToFollow.position;
+        }
+        transform.position = newPos;
     }
 }
