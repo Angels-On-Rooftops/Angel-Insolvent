a0ac57c17b294ea4a348079df6b4fc2875da9fde
jidaprano <joseph.daprano@cadtalk.com>
2024-11-23 05:30:10 -0500

    characterMovement inputs centralized

---
+++ Assets/Scripts/GameManagement/InputBindsHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputBindsHandler
+{
+    private static InputBindsHandler instance = null;
+    private static readonly object instanceLock = new object(); //thread-safe for co-routines
+
+    private InputBinds inputBinds;
+
+    InputBindsHandler()
+    {
+        inputBinds = new InputBinds();
+    }
+
+    public static InputBindsHandler Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new InputBindsHandler();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public InputAction FindBind(string bindName)
+    {
+        return inputBinds.FindAction(bindName);
+    }
+}
